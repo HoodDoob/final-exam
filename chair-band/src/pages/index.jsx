f4230071657bfa64122d9ed76b0191a907442ee8
Panni Belevski <pannibel@gmail.com>
2023-05-16 12:22:08 +0200

    started scss

--- chair-band/src/pages/index.jsx
+++ chair-band/src/pages/index.jsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
-import styles from "../styles/page.module.css";
+import styles from "../styles/page.module.scss";
 import Link from "next/link";
+//import Layout from "../components/layout"
 
 export default function Home() {
   return (
+    <>
     <div className={styles.container}>
       <Head>
         <title>Chair Website</title>
@@ -11,10 +13,13 @@ export default function Home() {
       </Head>
 
       <main>
+        <h1>Chair</h1>
+        <h2>website</h2>
         <h1 className={styles.title}>
-          <Link href="/newrelease">OUT NOW</Link>
+          new rlease here<Link href="/newrelease">OUT NOW</Link>
         </h1>
       </main>
     </div>
+    </>
   );
 }
