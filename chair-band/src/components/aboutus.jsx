b8394ecb962090ab6195153b0a66b24f3aa35fb1
Panni Belevski <pannibel@gmail.com>
2023-05-16 15:48:58 +0200

    aboutus basic setup without images

--- chair-band/src/components/aboutus.jsx
+++ chair-band/src/components/aboutus.jsx
@@ -13,7 +13,7 @@ export default function AboutUs() {
         </Head>
 
         <div>
-            <img src="" alt="" />
+          <img src="" alt="" />
         </div>
 
         <div className={styles.aboutus_div}>
@@ -22,8 +22,28 @@ export default function AboutUs() {
             <h2>(the band called Chair)</h2>
           </div>
 
+          <div className={styles.content_div}>
+            <p>
+              Chair is an international duo of multimedia artists, Cura and
+              Hubert Kurkiewicz. In their works, they include observations of
+              everyday life lined with extraordinary humor. They say that they
+              make music for the attention and to feel appreciated. From the
+              beginning of their existence, they have paid special attention to
+              the visual aspect of their work, confidently using social media
+              and appearing within art galleries.
+            </p>
 
+            <img src="" alt="" />
 
+            <p>
+              In June 2022, a year after the band was founded, they debut with
+              the EP “Po Co Muzyka” (“Why bother with music”, eng.). The
+              mini-album, despite being quite absurd, was met with a very
+              positive feedback, becoming the album of the week of most Polish
+              radio stations, thus opening the way to the largest festivals in
+              Poland (Off Festival, Meskie Granie, Soundrive, Great September).
+            </p>
+          </div>
         </div>
       </div>{" "}
     </Layout>
