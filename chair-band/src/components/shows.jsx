91f0485d90e9eec269bc55ceb1094f38af111e1e
Panni Belevski <pannibel@gmail.com>
2023-05-18 16:23:57 +0200

    shows started

--- chair-band/src/components/shows.jsx
+++ chair-band/src/components/shows.jsx
@@ -1,14 +1,17 @@
 import styles from "../styles/page.module.scss";
+import Image from "next/image";
+import shows_1 from "../public/images/shows.png";
 import Link from "next/link";
 
 export default function Shows() {
   return (
-    <div className={styles.container}>
-      <div>
-        <img src="" alt="" />
-      </div>
+    <div className={styles.shows_div}>
+      <div className={styles.container}>
 
-      <div className={styles.shows_div}>
+      <div className={styles.imgcont}>
+          <Image priority src={shows_1} alt="" />
+        </div>
+        
         <div className={styles.title}>
           <h1>shows</h1>
           <h2>(gigs, concerts, weddings)</h2>
