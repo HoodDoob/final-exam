ca2c849747e484f926788ee5b1b8768b0e589740
HoodDoob <hubert.kurkiewicz@gmail.com>
2023-05-20 14:40:27 +0200

    gloryhole

--- chair-band/src/components/gloryhole.jsx
+++ chair-band/src/components/gloryhole.jsx
@@ -7,18 +7,23 @@ import Link from "next/link";
 
 export default function GloryHole() {
   return (
-    <div className={styles.gloryhole_div}>
+    <div className={styles.aboutus_div}>
+      <div className={styles.title}>
+        <h1>GloryHole®</h1>
+        <h2>(your secrets are safe with us)</h2>
+      </div>
       <div className={styles.text_div}>
         <p>
           GloryHole® is a system of anonymous messages that you can send
           directly to our band. It sdiujk sdsdgjjsdg sdjsdgkjsd gou asofiuafd .
-          avpifuipuaf ipiudfg i ipu. Did you hate our last performance? Want to
-          know our favourite type of furniture? Are you feeling bored, lonely
-          and it’s after 10pm? Let us know what you’re up for.
+          avpifuipuaf ipiudfg i ipu. <li>Did you hate our last performance?</li>{" "}
+          <li> Want to know our favourite type of furniture?</li>{" "}
+          <li>Are you feeling bored, lonely and it’s after 10pm?</li>
+          <span>Let us know what you’re up for.</span>
         </p>
       </div>
       {/* form below */}
-      <form className="inputForm">
+      <form className={styles.inputForm}>
         <div className="form-control">
           <label htmlFor="form-name">Your name (fake one, please)</label>
           <input
@@ -33,26 +38,21 @@ export default function GloryHole() {
 
         <div className="form-control">
           <label htmlFor="form-email">Your anonymous message to Chair</label>
-          <input
+          <textarea className={styles.ghMessage} name="obs"></textarea>
+          {/* <input
+            className={styles.ghMessage}
             // defaultValue="hello"
             type="text"
             name="message"
-            id="form-message"
+            id={"form-message"}
             placeholder="Dear Chair, I've been a good good person this year."
             // onInput={popNumber}
-          />
+          /> */}
         </div>
-        <button>Send</button>
+        <button className={styles.ghButt}>Send</button>
       </form>
       <div className={styles.imgcont}>
         <Image priority src={abutus_1} alt="" />
-      </div>
-
-      <div className={styles.gloryhole_div}>
-        <div className={styles.title}>
-          <h1>GloryHole®</h1>
-          <h2>(your secrets are safe with us)</h2>
-        </div>
       </div>
     </div>
   );
