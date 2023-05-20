fc6a7acbe5ff29a5a62d61458616c5e86c01e9d2
HoodDoob <hubert.kurkiewicz@gmail.com>
2023-05-20 15:29:03 +0200

    sdf

--- chair-band/src/components/gloryhole.jsx
+++ chair-band/src/components/gloryhole.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export default function GloryHole() {
   return (
-    <div className={styles.aboutus_div}>
+    <div className={styles.gloryHole_div}>
       <div className={styles.title}>
         <h1>GloryHole®</h1>
         <h2>(your secrets are safe with us)</h2>
@@ -16,9 +16,14 @@ export default function GloryHole() {
         <p>
           GloryHole® is a system of anonymous messages that you can send
           directly to our band. It sdiujk sdsdgjjsdg sdjsdgkjsd gou asofiuafd .
-          avpifuipuaf ipiudfg i ipu. <li>Did you hate our last performance?</li>{" "}
-          <li> Want to know our favourite type of furniture?</li>{" "}
-          <li>Are you feeling bored, lonely and it’s after 10pm?</li>
+          avpifuipuaf ipiudfg i ipu.<br></br>
+          <br></br>
+          <div id={styles.ghList}>
+            <li>Did you hate our last performance?</li>
+            <li> Want to know our favourite type of furniture?</li>
+            <li>Are you feeling bored, lonely and it’s after 10pm?</li>
+          </div>
+          <br></br>
           <span>Let us know what you’re up for.</span>
         </p>
       </div>
