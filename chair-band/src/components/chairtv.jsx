ef6f31fdb42ca613a20af4dc84954904b3fa35fc
Panni Belevski <pannibel@gmail.com>
2023-05-20 21:13:42 +0200

    chair tv desktop done kinda

--- chair-band/src/components/chairtv.jsx
+++ chair-band/src/components/chairtv.jsx
@@ -1,26 +1,38 @@
 import styles from "../styles/page.module.scss";
+import Image from "next/image";
+import tv_1 from "../public/images/tv_1.png";
+import tv_2 from "../public/images/tv_2.png";
+import tv_3 from "../public/images/tv_3.png";
 import Link from "next/link";
 
 export default function ChairTV() {
   return (
-
-      <div className={`${styles.gridSetup} ${styles.grid2}`}>
-        <div className={styles.title}>
-          <h1>Chair TV</h1>
-          <h2>(no funny undertext this time)</h2>
-        </div>
-
-        <div className={styles.tvgrid}>
-          <div className={styles.tv} id={styles.tv_1}>
-
-          </div>
-          <div className={styles.tv} id={styles.tv_2}>
-            
-          </div>
-          <div className={styles.tv} id={styles.tv_3}>
-            
-          </div>
-        </div>
+    <div className={styles.chairTV}>
+      <div className={styles.title}>
+        <h1>Chair TV</h1>
+        <h2>(no funny undertext this time)</h2>
       </div>
+
+      <div className={styles.tv_container} id={styles.tv_1}>
+        <div className={styles.imgcont}>
+          <Image priority src={tv_2} alt="" />
+        </div>
+        <h2>clips</h2>
+      </div>
+
+      <div className={styles.tv_container} id={styles.tv_2}>
+        <div className={styles.imgcont}>
+          <Image priority src={tv_3} alt="" />
+        </div>
+        <h2>live shows</h2>
+      </div>
+
+      <div className={styles.tv_container} id={styles.tv_3}>
+        <div className={styles.imgcont}>
+          <Image priority src={tv_1} alt="" />
+        </div>
+        <h2>silly stuff</h2>
+      </div>
+    </div>
   );
 }
