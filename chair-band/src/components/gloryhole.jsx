921654ccd7e407e280fdf4d9d3a7180d25324d69
HoodDoob <hubert.kurkiewicz@gmail.com>
2023-05-20 11:45:40 +0200

    sorry for working in dev

--- chair-band/src/components/gloryhole.jsx
+++ chair-band/src/components/gloryhole.jsx
@@ -1,11 +1,51 @@
 import styles from "../styles/page.module.scss";
+import Image from "next/image";
+
+import abutus_1 from "../public/images/aboutus_1.jpg";
+
 import Link from "next/link";
 
 export default function GloryHole() {
   return (
-    <div className={styles.container}>
-      <div>
-        <img src="" alt="" />
+    <div className={styles.gloryhole_div}>
+      <div className={styles.text_div}>
+        <p>
+          GloryHole® is a system of anonymous messages that you can send
+          directly to our band. It sdiujk sdsdgjjsdg sdjsdgkjsd gou asofiuafd .
+          avpifuipuaf ipiudfg i ipu. Did you hate our last performance? Want to
+          know our favourite type of furniture? Are you feeling bored, lonely
+          and it’s after 10pm? Let us know what you’re up for.
+        </p>
+      </div>
+      {/* form below */}
+      <form className="inputForm">
+        <div className="form-control">
+          <label htmlFor="form-name">Your name (fake one, please)</label>
+          <input
+            // defaultValue="hello"
+            type="text"
+            name="name"
+            id="form-name"
+            // placeholder="How much time do you spend on Instagram daily?"
+            // onInput={popNumber}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="form-email">Your anonymous message to Chair</label>
+          <input
+            // defaultValue="hello"
+            type="text"
+            name="message"
+            id="form-message"
+            placeholder="Dear Chair, I've been a good good person this year."
+            // onInput={popNumber}
+          />
+        </div>
+        <button>Send</button>
+      </form>
+      <div className={styles.imgcont}>
+        <Image priority src={abutus_1} alt="" />
       </div>
 
       <div className={styles.gloryhole_div}>
