c3eb8f4a469fb16ad58d0b0aff27f66ea148722f
Panni Belevski <pannibel@gmail.com>
2023-05-22 17:43:27 +0200

    working hard

--- chair-band/src/components/clips.jsx
+++ chair-band/src/components/clips.jsx
@@ -1,23 +1,56 @@
 import styles from "../styles/page.module.scss";
+import { useState } from "react";
 import YouTubeEmbed from "./youtube";
 
 export default function Clips(props) {
-  const videoId = '1dDwdhgxZ8M';
+  const [popupState, setPopupState] = useState(false);
+  let videoId;
+  const mcGonagall = "1dDwdhgxZ8M";
+  const stringTheory = "n7cOlBxtKSo";
+
+  function openPopup(id) {
+    videoId = id;
+    console.log(videoId);
+    setPopupState(!popupState);
+    console.log("popup open", popupState);
+  }
 
   return (
     <div className={`${styles.gridSetup} ${styles.gridVideos}`}>
       <div className={styles.title}>
         <h1>clips</h1>
-        <button  onClick={() => props.changePage(5)}>back</button>
+        <button onClick={() => props.changePage(5)}>back</button>
       </div>
-      
-      <YouTubeEmbed videoId={videoId} />
-      <YouTubeEmbed videoId={videoId} />
-      <YouTubeEmbed videoId={videoId} />
-      <YouTubeEmbed videoId={videoId} />
-      <YouTubeEmbed videoId={videoId} />
-      <YouTubeEmbed videoId={videoId} />
 
+      <div onClick={() => openPopup(mcGonagall)} className={styles.thumbnailCont}>
+        <p>open video here</p>
+      </div>
+
+      <div onClick={() => openPopup(stringTheory)} className={styles.thumbnailCont}>
+        <p>open video here</p>
+      </div>
+
+      {popupState && (
+          <div
+          onClick={() => openPopup()}
+          className={styles.popupCont}>
+        <YouTubeEmbed
+          videoId={videoId}
+          popupState={popupState}
+          openPopup={openPopup}
+        />
+         </div>
+      )}
+
+      {/* <div
+        onClick={() => openPopup()}
+        className={popupState ? `${styles.popupCont}` : "hidden"}>
+       <YouTubeEmbed
+          videoId={videoId}
+          popupState={popupState}
+          openPopup={openPopup}
+        />
+      </div> */}
     </div>
   );
 }
--- chair-band/src/components/youtube.jsx
+++ chair-band/src/components/youtube.jsx
@@ -1,21 +1,24 @@
 import styles from "../styles/page.module.scss";
-import YouTube from 'react-youtube';
+import YouTube from "react-youtube";
 //npm install react-youtube
 
+export default function YouTubeEmbed(props) {
+  // YouTube options (e.g., player size, autoplay, etc.)
+  const opts = {
+    height: "",
+    width: "",
+    playerVars: {
+      autoplay: 2,
+      controls: 0, // Disable player controls
+      showinfo: 0, // Hide video title and uploader info
+      modestbranding: 0, // Hide YouTube logo
+      fs: 0, // Disable fullscreen button
+    },
+  };
 
-export default function YouTubeEmbed({videoId}) {
-        // YouTube options (e.g., player size, autoplay, etc.)
-        const opts = {
-          height: '',
-          width: '',
-          playerVars: {
-            autoplay: 1,
-            controls: 0, // Disable player controls
-            showinfo: 0, // Hide video title and uploader info
-            modestbranding: 0, // Hide YouTube logo
-            fs: 0, // Disable fullscreen button
-          },
-        };
-
-        return <YouTube videoId={videoId} opts={opts} className={styles.video}/>;
-} 
+  return (
+  
+      <YouTube videoId={props.videoId} opts={opts} />
+   
+  );
+}
