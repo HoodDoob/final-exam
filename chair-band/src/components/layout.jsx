409e8411043de14349489dff508025b6561219ed
Panni Belevski <pannibel@gmail.com>
2023-06-01 15:48:18 +0200

    navbar burger fixed

--- chair-band/src/components/layout.jsx
+++ chair-band/src/components/layout.jsx
@@ -21,7 +21,6 @@ export default function Layout(props) {
   const [confirmation, setConfirmation] = useState(false);
   const [email, setEmail] = useState();
   const { width, height } = useWindowDimensions();
-  const [isVisible, setIsVisible] = useState(false);
 
   // newsletter stuff
   // const mailchimp = require("@mailchimp/mailchimp_marketing");
@@ -86,8 +85,7 @@ export default function Layout(props) {
         <motion.div
           initial={{ opacity: 1, y: -100, zIndex: 10 }}
           animate={{ opacity: 1, y: 0, zIndex: 50 }}
-          transition={{ duration: 1.5 }}
-        >
+          transition={{ duration: 1.5 }}>
           <div className="fixed">
             <nav>
               {/* <motion.div
@@ -98,26 +96,22 @@ export default function Layout(props) {
               <div className="navSocials">
                 <a
                   href="https://www.facebook.com/Chair.The.Band"
-                  target="_blank"
-                >
+                  target="_blank">
                   <Image priority src={fbIcon} alt="Facebook" />
                 </a>{" "}
                 <a
                   href="https://www.instagram.com/kocham.chair/"
-                  target="_blank"
-                >
+                  target="_blank">
                   <Image priority src={igIcon} alt="Instagram" />
                 </a>
                 <a
                   href="https://open.spotify.com/artist/1Lo9afReVMAV0luzxUdHX1?si=XH4TbMElSGuCJ1-CJdFR0g"
-                  target="_blank"
-                >
+                  target="_blank">
                   <Image priority src={spIcon} alt="Spotify" />
                 </a>{" "}
                 <a
                   href="https://www.tiktok.com/@chair.band?_t=8cVT9ADObSk&"
-                  target="_blank"
-                >
+                  target="_blank">
                   <Image priority src={ttIcon} alt="Tik Tok" />
                 </a>
               </div>
@@ -129,8 +123,7 @@ export default function Layout(props) {
                     className={`navLogo ${
                       props.pageState == 1 ? "invisible" : ""
                     }`}
-                    onClick={() => props.changePage(1)}
-                  >
+                    onClick={() => props.changePage(1)}>
                     Chair
                   </div>
                   <div id="navbarCont" className="">
@@ -138,26 +131,22 @@ export default function Layout(props) {
                       <ul id="navbar">
                         <li
                           onClick={() => props.changePage(1)}
-                          className={props.pageState == 1 ? "weAreHere" : ""}
-                        >
+                          className={props.pageState == 1 ? "weAreHere" : ""}>
                           home
                         </li>
                         <li
                           onClick={() => props.changePage(2)}
-                          className={props.pageState == 2 ? "weAreHere" : ""}
-                        >
+                          className={props.pageState == 2 ? "weAreHere" : ""}>
                           about us
                         </li>
                         <li
                           onClick={() => props.changePage(3)}
-                          className={props.pageState == 3 ? "weAreHere" : ""}
-                        >
+                          className={props.pageState == 3 ? "weAreHere" : ""}>
                           shows
                         </li>
                         <li
                           onClick={() => props.changePage(4)}
-                          className={props.pageState == 4 ? "weAreHere" : ""}
-                        >
+                          className={props.pageState == 4 ? "weAreHere" : ""}>
                           contact
                         </li>
                         <li
@@ -169,14 +158,12 @@ export default function Layout(props) {
                             props.pageState == 9
                               ? "weAreHere"
                               : ""
-                          }
-                        >
+                          }>
                           ChairTV
                         </li>
                         <li
                           onClick={() => props.changePage(6)}
-                          className={props.pageState == 6 ? "weAreHere" : ""}
-                        >
+                          className={props.pageState == 6 ? "weAreHere" : ""}>
                           GloryHole®
                         </li>
                         <li>
@@ -187,8 +174,7 @@ export default function Layout(props) {
                         <li>
                           <a
                             href="https://art2musicshop.pl/pl/p/Chair-Po-Co-Muzyka-CD/239"
-                            target="_blank"
-                          >
+                            target="_blank">
                             store
                           </a>
                         </li>
@@ -213,33 +199,13 @@ export default function Layout(props) {
                     className={`navLogo ${
                       props.pageState == 1 ? "hidden" : ""
                     }`}
-                    onClick={() => props.changePage(1)}
-                  >
+                    onClick={() => props.changePage(1)}>
                     Chair
                   </div>
-                  <button
-                    onClick={() => props.openBurger()}
-                    className={props.burgerState ? "hidden" : "navBurger"}
-                    button-name="klosburg"
-                  >
-                    <Image priority src={burger_icon} alt="" />
-                  </button>
 
                   <AnimatePresence>
                     {props.burgerState && (
                       <>
-                        {/* Language switch */}
-                        <div className="navLang">
-                          <div className="greyedOut">PL /</div>
-                          <div>/ ENG</div>
-                        </div>
-                        <button
-                          onClick={() => props.openBurger()}
-                          className="navBurger"
-                          button-name="klosburg"
-                        >
-                          <Image priority src={closed_burger} alt="" />
-                        </button>
                         {/* BURGER MENU */}
 
                         <motion.div
@@ -248,8 +214,7 @@ export default function Layout(props) {
                           exit={{ y: 1000 }}
                           transition={{ duration: 0.3 }}
                           id="burgerMenuCont"
-                          className=""
-                        >
+                          className="">
                           <ul id="burgerMenu">
                             <li onClick={() => props.changePage(1)}>home</li>
                             <li onClick={() => props.changePage(2)}>
@@ -264,11 +229,26 @@ export default function Layout(props) {
                             <li>ChairGame</li>
                             <li>store</li>
                             <li>BOOK US!</li>
+                            {/* Language switch */}
+                            <div className="navLang">
+                              <div className="greyedOut">PL /</div>
+                              <div>/ ENG</div>
+                            </div>
                           </ul>
                         </motion.div>
                       </>
                     )}
                   </AnimatePresence>
+                  <button
+                    onClick={() => props.openBurger()}
+                    className="navBurger"
+                    button-name="klosburg">
+                    {props.openBurger ? (
+                      <Image src={burger_icon} alt="" />
+                    ) : (
+                      <Image src={closed_burger} alt="" />
+                    )}
+                  </button>
                 </>
               )}
             </nav>
@@ -278,8 +258,7 @@ export default function Layout(props) {
           initial={{ opacity: 0, y: -100, zIndex: 0 }}
           animate={{ opacity: 1, y: 0, zIndex: 4 }}
           transition={{ duration: 2.5 }}
-          className="nav_gradient"
-        ></motion.div>
+          className="nav_gradient"></motion.div>
       </header>
 
       <main>{props.children}</main>
@@ -291,8 +270,7 @@ export default function Layout(props) {
           <motion.div
             initial={{ y: 200 }}
             animate={{ y: 0 }}
-            transition={{ duration: 1.5 }}
-          >
+            transition={{ duration: 1.5 }}>
             {confirmation ? (
               <div className={styles.confirmationCont}>
                 <div>
@@ -301,8 +279,7 @@ export default function Layout(props) {
                   {confirmation && (
                     <button
                       onClick={handleButtonClick}
-                      className={styles.ghButt}
-                    >
+                      className={styles.ghButt}>
                       Perfect, thank you
                     </button>
                   )}
@@ -325,8 +302,7 @@ export default function Layout(props) {
             initial={{ y: 200 }}
             animate={{ y: 0 }}
             transition={{ duration: 1.5 }}
-            className={styles.widget}
-          >
+            className={styles.widget}>
             {/* Spotify Widget */}
             <iframe
               title="myFrame"
@@ -336,8 +312,7 @@ export default function Layout(props) {
               frameBorder="0"
               allowFullScreen=""
               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy"
-            ></iframe>
+              loading="lazy"></iframe>
           </motion.div>
         </footer>
       )}
